27f3f5247c0c4d4a8eab4978ee914a5e05423729
AbeerZahid917 <27100151@lums.edu.pk>
2025-05-20 15:49:55 +0500

    video 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; // this was a picture
 import './App.css';
 
-function App() {
+
+let name = "abeer"; 
+
+function App() 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <nav> 
+        <li> Home </li>
+        <li> About </li>
+        <li> Cntact </li>
+      </nav>
+
+      <div className="container"> 
+        <h1>
+          hello {name}
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non dolores ab fugit. Veritatis, tempora ducimus ad totam vitae quis dignissimos molestiae, recusandae, quo velit repellendus? Fugiat veritatis mollitia pariatur doloribus!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Abeer
-        </a>
-      </header>
-    </div>
+      </div>
+
+    </>
+    // they seem to be bullet points
+    // heading using the var before the func
+    // random text generator
+    // <div className="blank">lovely</div> // check the .blank in the css file
   );
 }
 

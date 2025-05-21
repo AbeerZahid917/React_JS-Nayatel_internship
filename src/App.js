4b857ee53f85a1971040720e44c49fbe8982e90e
AbeerZahid917 <27100151@lums.edu.pk>
2025-05-21 10:08:37 +0500

    part 5 part 2

--- src/App.js
+++ src/App.js
@@ -3,33 +3,47 @@ import './App.css';
 import React from 'react';
 
 
-
-let name = "abeer"; 
-
 function App() 
 {
   return (
     <>
-      <nav> 
-        <li> Home </li>
-        <li> About </li>
-        <li> Cntact </li>
-      </nav>
-
-      <div className="container"> 
-        <h1>
-          hello {name}
-        </h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non dolores ab fugit. Veritatis, tempora ducimus ad totam vitae quis dignissimos molestiae, recusandae, quo velit repellendus? Fugiat veritatis mollitia pariatur doloribus!
-        </p>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="/">Action</a></li>
+                <li><a className="dropdown-item" href="/">Another action</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="/">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </div>
-
+    </nav>
     </>
-    // they seem to be bullet points
-    // heading using the var before the func
-    // random text generator
-    // <div className="blank">lovely</div> // check the .blank in the css file
   );
 }
 
